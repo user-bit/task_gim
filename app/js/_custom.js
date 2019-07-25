e9d192c91ef8aa07b526a7df7a9342b5da6f7914
Roma <user.sparkle@gmail.com>
2019-07-26 01:22:05 +0300

    finish project

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -3,5 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
         $('.language_block a').removeClass('active');
         $(this).addClass('active');
     });
-
+    $('.xs-menu').on('click', function () {
+        $(this).toggleClass('active');
+        $('.top_menu').toggleClass('active');
+    })
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            $('.top_block').addClass('fixed_menu');
+        } else {
+            $('.top_block').removeClass('fixed_menu');
+        }
+    });
 });
